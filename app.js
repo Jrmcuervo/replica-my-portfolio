fb9000cfc6574b0a7a6ce86dbf78cb050d70c2a1
Rubén Cuervo Madrigal <jrmcuervom@gmail.com>
2023-01-31 14:13:05 -0600

    Fixed linters

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const open = document.getElementById('btnOpen');
-const close = document.getElementById('btnClose')
+const close = document.getElementById('btnClose');
 
-open.addEventListener('click', (event) => {
-    close.style.display = 'block'
-
+open.addEventListener('click', () => {
+  close.style.display = 'block';
 });
 
-close.addEventListener('click', (event) => {
-    close.style.display = 'none'
-
-})
+close.addEventListener('click', () => {
+  close.style.display = 'none';
+});
