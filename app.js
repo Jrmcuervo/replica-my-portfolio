ef77ab7c45268cc1234d68dbee4a8983eaa6d19b
Rubén Cuervo Madrigal <jrmcuervom@gmail.com>
2023-01-31 13:14:51 -0600

    Added menu functionality created classes and internal links

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
-var closeBtn = document.getElementsByClassName('menu__close');
-var openBtn = document.getElementsByClassName('menu__open');
-var span = document.getElementsByClassName('material-symbols-outlined')[0];
+const open = document.getElementById('btnOpen');
+const close = document.getElementById('btnClose')
 
-openBtn.onclick = function () {
-    closeBtn.style.display = 'block';
-};
+open.addEventListener('click', (event) => {
+    close.style.display = 'block'
 
-span.onclick = function () {
-    closeBtn.style.display = 'block';
-};
+});
 
-window.onclick = function (event) {
-    if (event.target == closeBtn) {
-        closeBtn.style.display = 'none';
-    }
-};
+close.addEventListener('click', (event) => {
+    close.style.display = 'none'
 
-// menu__open.addEventListener('click', () => {
-//     menu__close.classList.toggle('open');
-// });
+})
