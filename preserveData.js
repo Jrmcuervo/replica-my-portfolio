f7b45d745cc9305a6fcc820c41c1d4e1f019643f
dfsalam <dfsalam@gmail.com>
2023-02-04 18:41:37 -0500

    Add function saveToLocal

--- preserveData.js
+++ preserveData.js
@@ -3,19 +3,23 @@ const nameData = document.getElementById('name');
 const emailData = document.getElementById('mail');
 const messageData = document.getElementById('msg');
 
+function saveToLocal (storeData) {
+  localStorage.setItem('info', JSON.stringify(storeData));
+}
+
 nameData.addEventListener('change', (event) => {
   storeData.name = event.target.value;
-  localStorage.setItem('info', JSON.stringify(storeData));
+  saveToLocal(storeData);
 });
 
 emailData.addEventListener('change', (event) => {
   storeData.mail = event.target.value;
-  localStorage.setItem('info', JSON.stringify(storeData));
+  saveToLocal(storeData);
 });
 
 messageData.addEventListener('change', (event) => {
   storeData.msg = event.target.value;
-  localStorage.setItem('info', JSON.stringify(storeData));
+  saveToLocal(storeData);
 });
 
 const localStorageData = JSON.parse(localStorage.getItem('info'));
